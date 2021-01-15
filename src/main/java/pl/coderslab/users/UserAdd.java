a179a54b88470b977b0f1b6dd9c19efa67ee8526
Zawada992 <r.zawadzki92@gmail.com>
2021-01-15 20:34:59 +0100

    Work add

---
+++ src/main/java/pl/coderslab/users/UserAdd.java
@@ -0,0 +1,30 @@
+package pl.coderslab.users;
+
+import pl.coderslab.utils.User;
+import pl.coderslab.utils.UserDao;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet("/user/add")
+public class UserAdd extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/user/add.jsp")
+            .forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User user = new User();
+        user.setUsername(request.getParameter("userName"));
+        user.setEmail(request.getParameter("userEmail"));
+        user.setPassword(request.getParameter("userPassword"));
+        UserDao userDao = new UserDao();
+        userDao.createUser(user);
+        response.sendRedirect("/user/list");
+
+    }
+}
